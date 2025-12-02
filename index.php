45bb0503f2ecff099f3e1028b6c89d91e5ea630a
bilal <bilalbakessou@gmail.com>
2025-12-02 17:27:03 +0100

    rotage logic

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php  
+      $pags =['home','services','about','contact'];
+  
+      if (isset($_GET['page']) && !in_array($_GET['page'],$pags)){
+          header("Location: views/404.view.php");
+          exit();
+      }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,5 +25,8 @@
        include('templates/footer.temp.php');
     
     ?>
+
+    <audio src="assets/ref.mp3" autoplay loop></audio>
+
 </body>
 </html>
--- templates/header.temp.php
+++ templates/header.temp.php
@@ -1,3 +1,4 @@
+
 <header class="bg-white shadow-md">
     <nav class="container mx-auto flex justify-between items-center py-4 px-[5%]">
       <h1 class="text-2xl font-bold text-blue-600">DigitalWave</h1>
