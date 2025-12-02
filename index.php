cf811ad2d66cbe6fb39f4a62c1be545a5592349c
bilal <bilalbakessou@gmail.com>
2025-12-03 00:10:48 +0100

    change logic home rotage

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
-<?php  
-      $pags =['home','services','about','contact'];
+<?php 
+$page ;
+ if(!isset($_GET['page'])){
+    $page='home';
+ }else{
+    $page=$_GET['page'];
+ }
+
+
+$pags =['home','services','about','contact'];
   
-      if (isset($_GET['page']) && !in_array($_GET['page'],$pags)){
-          header("Location: views/404.view.php");
-          exit();
-      }
+if (!isset($_GET['page']) || in_array($_GET['page'],$pags)) {
+     include 'views/' . $page . '.view.php';
+}else {
+    $page='404';
+    include 'views/404.view.php';
+}
+
+$titleMap = [
+    'home' => 'NovaCraft Studio - Home',
+    'about' => 'À propos - NovaCraft Studio',
+    'services' => 'Services - NovaCraft Studio',
+    'contact' => 'Contact - NovaCraft Studio',
+    '404' => 'Page non trouvée - NovaCraft Studio'
+];
+$title = $titleMap[$page];
+
+
 ?>
-<!DOCTYPE html>
+
+
+<?php  
+    //   $pags =['home','services','about','contact'];
+  
+    //   if (isset($_GET['page']) && !in_array($_GET['page'],$pags)){
+    //       header("Location: views/404.view.php");
+    //       exit();
+    //   }
+?>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -14,19 +45,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="assets/output.css">
 </head>
-<body> 
+<body>  -->
      
     <?php 
     
-       include('templates/headerIndex.temp.php');
+    //    include('templates/headerIndex.temp.php');
 
-       include('templates/indexSection.temp.php');
+    //    include('templates/indexSection.temp.php');
        
-       include('templates/footer.temp.php');
+    //    include('templates/footer.temp.php');
     
     ?>
 
-    <audio src="assets/ref.mp3" autoplay loop></audio>
+    <!-- <audio src="assets/ref.mp3" autoplay loop></audio> -->
 
 </body>
 </html>
--- templates/header.temp.php
+++ templates/header.temp.php
@@ -3,10 +3,12 @@
     <nav class="container mx-auto flex justify-between items-center py-4 px-[5%]">
       <h1 class="text-2xl font-bold text-blue-600">DigitalWave</h1>
       <ul class="flex space-x-6">
-        <li><a href="../views/home.view.php?page=home" class="<?php echo$_GET['page']=='home'?'text-blue-600 font-medium':'hover:text-blue-600'?> ">Accueil</a></li>
-        <li><a href="../views/services.view.php?page=services" class="<?php echo$_GET['page']=='services'?'text-blue-600 font-medium':'hover:text-blue-600'?> ">Services</a></li>
-        <li><a href="../views/about.view.php?page=about" class="<?php echo$_GET['page']=='about'?'text-blue-600 font-medium':'hover:text-blue-600'?> ">À propos</a></li>
-        <li><a href="../views/contact.view.php?page=contact" class="<?php echo$_GET['page']=='contact'?'text-blue-600 font-medium':'hover:text-blue-600'?>  ">Contact</a></li>
+        <li><a href="?page=home" class=" ">Accueil</a></li>
+        <li><a href="?page=services" class="">Services</a></li>
+        <li><a href="?page=about" class="">À propos</a></li>
+        <li><a href="?page=contact" class="  ">Contact</a></li>
+
+   
       </ul>
     </nav>
   </header>
