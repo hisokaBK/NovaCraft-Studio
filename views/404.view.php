3644b99b0075e4d3eb9e141426cb22f54b40ab74
bilal <bilalbakessou@gmail.com>
2025-12-03 01:41:09 +0100

    404 error

--- views/404.view.php
+++ views/404.view.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/output.css">
     <title>Document</title>
 </head>
 <style>
          #p{
-               background-image: url('assets/jj.webp');
+               background-image: url('assets/jj.jpg');
                background-size: cover;
-               background-position: center;
                -webkit-background-clip: text;
                -webkit-text-fill-color: transparent;
                font-size: 100px;
@@ -18,19 +18,23 @@
 
          #sc{
             background-image: url('assets/gb.gif');
-            height: 500px;
+           
             background-repeat: no-repeat;
             background-size: cover;
-            width: 1000px;
+          
          }
 
 </style>
 <body>
 
-   <section id="sc" class="p-5 ">
+   <section id="sc" class="p-5 flex-col text-center w-screen h-screen">
          <p id='p' class="text-img  ">
-              Oops !
+              404
          </p>
+         <div>
+         <a href="?page=home" class=" text-gray-500 hover:text-gray-300">Accueil</a>
+         </div>
     </section>
+      
 </body>
 </html>
